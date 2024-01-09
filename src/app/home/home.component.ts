c3e678373bf9480f7390c70757e4b1b6784d79fd
Nikola Velikov <niki.velikov09@gmail.com>
2024-01-09 15:38:51 +0200

    fixing some stuff
    Signed-off-by: Nikola Velikov <niki.velikov09@gmail.com>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  authService!: AuthService;
 
+  constructor(authService: AuthService){
+    this.authService = authService;
+  }
 }
